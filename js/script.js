2535447974eb2cf797fd73da46d4c6c9e49882a0
jean elossy <jean.elossy@yahoo.com>
2021-08-07 19:21:22 +0200

    Fin du projet

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
 document.querySelector(".btn__right").addEventListener("click", collapse);
 
 function collapse(){
-  this.parentElement.parentElement.classList.toggle("collapse__l");
-  this.parentElement.parentElement.previousElementSibling.classList.toggle("collapse__r");
-
+  this.parentElement.parentElement.classList.toggle("collapse__left");
+  this.parentElement.parentElement.previousElementSibling.classList.toggle("collapse__right");
   setTimeout(() => {
     changeTitleRight();
     changeTitleLeft();
     addInput();
     changeHead();
     changeButtonContent();
-  }, 150);
+  }, 145);
 }
 
 function changeTitleRight(){
-  document.querySelector(".content__title__right").innerHTML = `<h2>Welcome Back!</h2>`;
+  document.querySelector(".content__title__right").children[0].classList.toggle("add");
+  document.querySelector(".content__title__right").children[1].classList.toggle("replace");
 }
+
 function changeTitleLeft(){
-  document.querySelector(".content__title__left").innerHTML = `<h2>Create Account</h2>`;
+  document.querySelector(".content__title__left").children[0].classList.toggle("add");
+  document.querySelector(".content__title__left").children[1].classList.toggle("replace");
 }
+
 function addInput(){
-  document.querySelector(".mdp__forget").innerHTML = `<input type="text" name="pseudo" id="pseudo" placeholder="Pseudo" required><br />`;
+  document.querySelector(".mdp__forget").children[0].classList.toggle("add");
+  document.querySelector(".mdp__forget").children[1].classList.toggle("visible");
 }
+
 function changeHead(){
-  document.querySelector(".coordonnee").textContent = "Inscription";
+  document.querySelector(".coordonnee").classList.toggle("add");
+  document.querySelector(".inscription").classList.toggle("visible");
 }
+
 function changeButtonContent(){
-  document.querySelector("button").textContent = "S'inscrire";
+  document.querySelector(".button").children[0].classList.toggle("add");
+  document.querySelector(".button").children[1].classList.toggle("visible");
 }
